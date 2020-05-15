7277a229b4fad41535e7bf3c2124a89a5ac0eea1
unknown <gotoerror00111011@gmail.com>
2020-05-15 09:42:17 +0900

    Create the app

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # KoreanHistory
+
+- 숫자 야구 게임 (Bulls and Cows) 을 한국사의 연표와 접목한 게임
+
---
+++ android/app/src/main/kotlin/com/example/korean_history/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.korean_history
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
